69ea673cb8d7283f1d15605327b304b13b12df98
Kat <turret24@gmail.com>
2018-08-29 15:50:50 -0500

    Change "school" to "campus"

--- src/main/java/com/boulder/cisd/core/district/Home.java
+++ src/main/java/com/boulder/cisd/core/district/Home.java
@@ -12,7 +12,7 @@ public class Home extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("school", "district");
+        req.setAttribute("campus", "district");
         req.setAttribute("page", "home");
         req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
