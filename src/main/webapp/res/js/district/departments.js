8b9f07e7850df713aceea741f13a9f61af47a63b
Kat <turret24@gmail.com>
2018-09-05 15:46:34 -0500

    Create Departments page scripts

---
+++ src/main/webapp/res/js/district/departments.js
@@ -0,0 +1,38 @@
+var sections = $('h5').get();
+
+for (var i in sections) {
+    var header = sections[i];
+    var link = document.createElement('a');
+    link.href = '#'.concat(header.textContent);
+    link.className = 'btn btn-primary';
+    link.textContent = header.textContent;
+    document.getElementById('directory-btn-group').appendChild(link);
+}
+
+$(document).ready(function() {
+    if (window.innerWidth < 768) {
+        collapse();
+    } else {
+        restore();
+    }
+});
+
+$(window).resize(function() {
+    if (window.innerWidth < 768) {
+        collapse();
+    } else {
+        restore();
+    }
+});
+
+function collapse() {
+    $('#directory-btn-group').addClass('m-auto pb-2 float-none');
+    $('#directory-header').addClass('text-center pt-2 pl-0');
+    $('#directory-columns').css('column-count', 1);
+}
+
+function restore() {
+    $('#directory-btn-group').removeClass('m-auto pb-2 float-none');
+    $('#directory-header').removeClass('text-center pt-2 pl-0');
+    $('#directory-columns').css('column-count', 3);
+}
