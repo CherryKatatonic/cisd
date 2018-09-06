1ac4754b29482722b56550e8ffd41ec87c9cdcbc
Kat <turret24@gmail.com>
2018-09-06 08:53:51 -0500

    Generate dynamic links for transparent navbar

--- src/main/webapp/res/js/district/home.js
+++ src/main/webapp/res/js/district/home.js
@@ -1,22 +1,42 @@
 var upcomingEventsCard = $('#upcomingEventsCard');
 var upcomingEventsButton = $('#upcomingEventsButton');
 var upcomingEventsHeader = $('#upcomingEventsHeader');
+var dynamicLinks =
+    $('<li class="nav-item active">\n' +
+    '    <a class="nav-link" href="/">HOME</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '    <a class="nav-link" href="/about">ABOUT US</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '    <a class="nav-link" href="/athletics">ATHLETICS</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '    <a class="nav-link" href="/departments">DEPARTMENTS</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '    <a class="nav-link" href="/community">PARENTS & COMMUNITY</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '    <a class="nav-link" href="/staff">STAFF</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '    <a class="nav-link" href="/calendar">CALENDAR</a>\n' +
+    '</li>');
 
-$(window).ready(function() {
-    if (window.innerWidth < 768) {
-        collapse();
-    } else {
-        restore();
-    }
-});
+$('#nav2-links').append(dynamicLinks);
 
-$(window).resize(function() {
-    if (window.innerWidth < 768) {
-        collapse();
-    } else {
-        restore();
-    }
-});
+if (window.innerWidth < 768) {
+    collapse();
+} else {
+    restore();
+}
+
+if (window.innerWidth < 768) {
+    collapse();
+} else {
+    restore();
+}
 
 function collapse() {
     upcomingEventsCard.appendTo($('#upcomingEventsCollapse'));
