9662e7afe93bf23e83288b86929ba939e600ede9
Kat <turret24@gmail.com>
2018-09-06 08:54:25 -0500

    Generate dynamic links for transparent navbar

--- src/main/webapp/res/js/district/about.js
+++ src/main/webapp/res/js/district/about.js
@@ -1,4 +1,28 @@
 var sections = $('h5').get();
+var dynamicLinks =
+    $('<li class="nav-item active">\n' +
+        '    <a class="nav-link" href="/">HOME</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/about">ABOUT US</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/athletics">ATHLETICS</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/departments">DEPARTMENTS</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/community">PARENTS & COMMUNITY</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/staff">STAFF</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/calendar">CALENDAR</a>\n' +
+        '</li>');
+
+$('#nav2-links').append(dynamicLinks);
 
 for (var i in sections) {
     var header = sections[i];
@@ -9,21 +33,17 @@ for (var i in sections) {
     document.getElementById('directory-btn-group').appendChild(link);
 }
 
-$(document).ready(function() {
-    if (window.innerWidth < 768) {
-        collapse();
-    } else {
-        restore();
-    }
-});
+if (window.innerWidth < 768) {
+    collapse();
+} else {
+    restore();
+}
 
-$(window).resize(function() {
-    if (window.innerWidth < 768) {
-        collapse();
-    } else {
-        restore();
-    }
-});
+if (window.innerWidth < 768) {
+    collapse();
+} else {
+    restore();
+}
 
 function collapse() {
     $('#directory-btn-group').addClass('m-auto pb-2 float-none');
