1920a93d77ea3c3ac063327cbfdd6a9e2749a54c
Kat <turret24@gmail.com>
2018-09-10 10:32:59 -0500

    Move dynamic links to JSP

--- src/main/webapp/res/js/district/home.js
+++ src/main/webapp/res/js/district/home.js
@@ -1,30 +1,6 @@
 var upcomingEventsCard = $('#upcomingEventsCard');
 var upcomingEventsButton = $('#upcomingEventsButton');
 var upcomingEventsHeader = $('#upcomingEventsHeader');
-var dynamicLinks =
-    $('<li class="nav-item active">\n' +
-    '    <a class="nav-link" href="/">HOME</a>\n' +
-    '</li>\n' +
-    '<li class="nav-item">\n' +
-    '    <a class="nav-link" href="/about">ABOUT US</a>\n' +
-    '</li>\n' +
-    '<li class="nav-item">\n' +
-    '    <a class="nav-link" href="/athletics">ATHLETICS</a>\n' +
-    '</li>\n' +
-    '<li class="nav-item">\n' +
-    '    <a class="nav-link" href="/departments">DEPARTMENTS</a>\n' +
-    '</li>\n' +
-    '<li class="nav-item">\n' +
-    '    <a class="nav-link" href="/community">PARENTS & COMMUNITY</a>\n' +
-    '</li>\n' +
-    '<li class="nav-item">\n' +
-    '    <a class="nav-link" href="/staff">STAFF</a>\n' +
-    '</li>\n' +
-    '<li class="nav-item">\n' +
-    '    <a class="nav-link" href="/calendar">CALENDAR</a>\n' +
-    '</li>');
-
-$('#nav2-links').append(dynamicLinks);
 
 if (window.innerWidth < 768) {
     collapse();
@@ -47,7 +23,7 @@ function collapse() {
 }
 
 function restore() {
-    upcomingEventsCard.appendTo($('#col-right'));
+    upcomingEventsCard.appendTo($('#col-sm'));
     upcomingEventsButton.addClass('d-none');
     upcomingEventsHeader.prependTo(upcomingEventsCard);
 }
