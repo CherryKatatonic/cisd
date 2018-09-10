c41da388bef10c03f69f79ef592af4b2308b8c87
Kat <turret24@gmail.com>
2018-09-10 08:38:25 -0500

    Create Staff scripts

---
+++ src/main/webapp/res/js/district/staff.js
@@ -0,0 +1,60 @@
+var sections = $('h5').get();
+var dynamicLinks =
+    $('<li class="nav-item active">\n' +
+        '    <a class="nav-link" href="/">HOME</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/about">ABOUT US</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/athletics">ATHLETICS</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/departments">DEPARTMENTS</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/community">PARENTS & COMMUNITY</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/staff">STAFF</a>\n' +
+        '</li>\n' +
+        '<li class="nav-item">\n' +
+        '    <a class="nav-link" href="/calendar">CALENDAR</a>\n' +
+        '</li>');
+
+$('#nav2-links').append(dynamicLinks);
+
+for (var i in sections) {
+    var header = sections[i];
+    var link = document.createElement('a');
+    link.href = '#'.concat(header.textContent);
+    link.className = 'btn btn-primary';
+    link.textContent = header.textContent;
+    document.getElementById('directory-btn-group').appendChild(link);
+}
+
+if (window.innerWidth < 768) {
+    collapse();
+} else {
+    restore();
+}
+
+$(window).resize(function() {
+    if (window.innerWidth < 768) {
+        collapse();
+    } else {
+        restore();
+    }
+});
+
+function collapse() {
+    $('#directory-btn-group').addClass('m-auto pb-2 float-none');
+    $('#directory-header').addClass('text-center pt-2 pl-0');
+    $('#directory-columns').css('column-count', 1);
+}
+
+function restore() {
+    $('#directory-btn-group').removeClass('m-auto pb-2 float-none');
+    $('#directory-header').removeClass('text-center pt-2 pl-0');
+    $('#directory-columns').css('column-count', 3);
+}
