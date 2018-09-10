c0d7301f1597a9606697e11de5ffec344c3222be
Kat <turret24@gmail.com>
2018-09-10 10:32:54 -0500

    Move dynamic links to JSP

--- src/main/webapp/res/js/athletics/home.js
+++ src/main/webapp/res/js/athletics/home.js
@@ -1,46 +1,8 @@
 var upcomingEventsCard = $('#upcomingEventsCard');
 var upcomingEventsButton = $('#upcomingEventsButton');
 var upcomingEventsHeader = $('#upcomingEventsHeader');
-var dynamicLinks =
-    $('<li class="nav-item active">\n' +
-        '    <a class="nav-link" href="/athletics">HOME</a>\n' +
-        '</li>\n' +
-        '<li class="nav-item dropdown">\n' +
-        '    <a class="nav-link" href="#" id="aboutDropdown"' +
-        '        role="button" aria-haspopup="true" aria-expanded="false">ABOUT US</a>\n' +
-        '    <div class="dropdown-menu" aria-labelledby="aboutDropdown">' +
-        '        <a class="dropdown-item" href="/athletics/about">Athletic Administration</a>' +
-        '        <a class="dropdown-item" href="#">Sports Medicine - Athletic Training Information</a>' +
-        '        <a class="dropdown-item" href="#">Advertising at Tiger Stadium</a>' +
-        '        <a class="dropdown-item" href="#">Meet the CHS Coaches</a>' +
-        '        <a class="dropdown-item" href="#">Meet the CMS Coaches</a>' +
-        '    </div>' +
-        '</li>\n' +
-        '<li class="nav-item dropdown">\n' +
-        '    <a class="nav-link" href="#" id="boosterClubDropdown"' +
-        '        role="button" aria-haspopup="true" aria-expanded="false">BOOSTER CLUB</a>\n' +
-        '    <div class="dropdown-menu" aria-labelledby="boosterClubDropdown">' +
-        '        <a class="dropdown-item" href="#">Booster Club</a>' +
-        '        <a class="dropdown-item" href="#">UIL Booster Club Guidelines</a>' +
-        '</li>\n' +
-        '<li class="nav-item">\n' +
-        '    <a class="nav-link" href="/athletics/facilities">FACILITIES</a>\n' +
-        '</li>\n' +
-        '<li class="nav-item">\n' +
-        '    <a class="nav-link" href="https://corsicanaisd.rankonesport.com/New/Home.aspx">FORMS</a>\n' +
-        '</li>\n' +
-        '<li class="nav-item">\n' +
-        '    <a class="nav-link" href="/athletics/schedules">SCHEDULES</a>\n' +
-        '</li>\n' +
-        '<li class="nav-item">\n' +
-        '    <a class="nav-link" href="/athletics/sports">SPORTS</a>\n' +
-        '</li>' +
-        '<li class="nav-item">\n' +
-        '    <a class="nav-link" href="/athletics/calendar">CALENDAR</a>\n' +
-        '</li>');
 
 $('#brand-name-top').text('CORSICANA ATHLETICS');
-$('#nav2-links').append(dynamicLinks);
 
 if (window.innerWidth < 768) {
     collapse();
@@ -63,7 +25,7 @@ function collapse() {
 }
 
 function restore() {
-    upcomingEventsCard.appendTo($('#home-col-right'));
+    upcomingEventsCard.appendTo($('#col-sm'));
     upcomingEventsButton.addClass('d-none');
     upcomingEventsHeader.prependTo(upcomingEventsCard);
 }
