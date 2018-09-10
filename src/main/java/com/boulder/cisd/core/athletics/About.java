a23dacb69b57aadb6b7f3dec433e7f3c7157df74
Kat <turret24@gmail.com>
2018-09-10 10:30:25 -0500

    Create Athletics/About servlet

---
+++ src/main/java/com/boulder/cisd/core/athletics/About.java
@@ -0,0 +1,20 @@
+package com.boulder.cisd.core.athletics;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/athletics/about")
+public class About extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("/athletics/about");
+        req.setAttribute("campus", "athletics");
+        req.setAttribute("page", "about");
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+}
