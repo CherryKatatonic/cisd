704b27702cfff420500314c1d5efd433032e566e
Kat <turret24@gmail.com>
2018-09-12 08:18:14 -0500

    Create User datastore

---
+++ src/main/java/com/boulder/cisd/daos/UserDao.java
@@ -0,0 +1,36 @@
+package com.boulder.cisd.daos;
+
+import com.boulder.cisd.objects.User;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.Query;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+public class UserDao {
+
+    public User createUser(User user) {
+        Key<User> key = ofy().save().entity(user).now();
+        return ofy().load().key(key).now();
+    }
+
+    public User getUser(Long id) {
+        return ofy().load().key(Key.create(User.class, id)).now();
+    }
+
+    public User getUser(String email) {
+        Query<User> query = ofy().load().type(User.class);
+        User user = query.filter("email", email).first().now();
+
+        if (user != null) { return user; }
+        return null;
+    }
+
+    public User updateUser(User user) {
+        ofy().save().entity(user).now();
+        return user;
+    }
+
+    public void deleteUser(Long id) {
+        ofy().delete().key(Key.create(User.class, id)).now();
+    }
+}
