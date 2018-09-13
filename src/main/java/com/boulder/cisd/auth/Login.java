d8504ba460ad7d24eb5f6dbadb7a07093200d1aa
Kat <turret24@gmail.com>
2018-09-13 10:51:12 -0500

    Create Login servlet

--- src/main/java/com/boulder/cisd/auth/Login.java
+++ src/main/java/com/boulder/cisd/auth/Login.java
@@ -1,10 +1,14 @@
 package com.boulder.cisd.auth;
 
+import com.boulder.cisd.daos.UserDao;
+import com.boulder.cisd.objects.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
@@ -17,6 +21,34 @@ public class Login extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        UserDao dao = (UserDao) this.getServletContext().getAttribute("userDao");
+        HttpSession ses = req.getSession();
+        String referrer = req.getHeader("referer");
+        String email = req.getParameter("email");
+        User user = dao.getUser(email);
+
+        if (user == null) {
+            ses.setAttribute("loginEmail", email);
+            ses.setAttribute("loginError", "User account not found");
+            ses.setAttribute("script", "$('#loginDropdown').click(); $('#loginTab').click();");
+            resp.sendRedirect(referrer);
+            return;
+        }
+
+        try {
+            if (!PasswordStorage.verifyPassword(req.getParameter("password"), user.getPassword())) {
+                ses.setAttribute("loginEmail", email);
+                ses.setAttribute("loginError", "Invalid password");
+                ses.setAttribute("script", "$('#loginDropdown').click(); $('#loginTab').click();");
+                resp.sendRedirect(referrer);
+                return;
+            }
+        } catch (PasswordStorage.CannotPerformOperationException | PasswordStorage.InvalidHashException e) {
+            e.printStackTrace();
+        }
+
+        ses.setMaxInactiveInterval(60*60);
+        ses.setAttribute("user", user);
+        resp.sendRedirect(req.getParameter(referrer));
     }
 }
