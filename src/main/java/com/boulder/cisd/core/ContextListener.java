9051bd24222c93bec168c09f87894f436a29e807
Kat <turret24@gmail.com>
2018-09-13 10:47:41 -0500

    Create Context Listener

--- src/main/java/com/boulder/cisd/core/ContextListener.java
+++ src/main/java/com/boulder/cisd/core/ContextListener.java
@@ -1,6 +1,8 @@
 package com.boulder.cisd.core;
 
+import com.boulder.cisd.auth.PasswordStorage;
 import com.boulder.cisd.daos.UserDao;
+import com.boulder.cisd.objects.Hash;
 import com.boulder.cisd.objects.User;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.googlecode.objectify.ObjectifyFactory;
@@ -24,9 +26,20 @@ public class ContextListener implements ServletContextListener {
         ));
 
         ObjectifyService.register(User.class);
+        ObjectifyService.register(Hash.class);
 
         UserDao userDao = new UserDao();
 
         e.getServletContext().setAttribute("userDao", userDao);
+
+        ObjectifyService.run(() -> {
+            try {
+                Hash adminPass = new Hash("adminPass", PasswordStorage.createHash("Tigers@dm1n337"));
+            } catch (PasswordStorage.CannotPerformOperationException e1) {
+                e1.printStackTrace();
+            }
+            return null;
+        });
+
     }
 }
