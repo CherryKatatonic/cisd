e0bf43e3a2ed08390b145b064bff8a7afdbc6527
Kat <turret24@gmail.com>
2018-09-17 14:08:32 -0500

    Create Calendar Dao

---
+++ src/main/java/com/boulder/cisd/daos/CalendarDao.java
@@ -0,0 +1,21 @@
+package com.boulder.cisd.daos;
+
+import com.boulder.cisd.objects.Calendar;
+import com.googlecode.objectify.Key;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+public class CalendarDao {
+
+    public Calendar getCalendar(String id) {
+        return ofy().load().key(Key.create(Calendar.class, id)).now();
+    }
+
+    public Calendar createCalendar(Calendar cal) {
+        Key<Calendar> key = ofy().save().entity(cal).now();
+        return ofy().load().key(key).now();
+    }
+
+
+
+}
