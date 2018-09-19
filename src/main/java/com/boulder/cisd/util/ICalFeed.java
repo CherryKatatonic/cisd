5d41f441d6a4216dddbb944a25f01ae3e73b8983
Kat <turret24@gmail.com>
2018-09-19 10:18:17 -0500

    Return JSON calendar feed

--- src/main/java/com/boulder/cisd/util/ICalFeed.java
+++ src/main/java/com/boulder/cisd/util/ICalFeed.java
@@ -2,6 +2,7 @@ package com.boulder.cisd.util;
 
 import biweekly.Biweekly;
 import biweekly.ICalendar;
+import biweekly.component.VEvent;
 import com.boulder.cisd.daos.CalendarDao;
 import com.boulder.cisd.objects.Calendar;
 import com.google.gson.Gson;
@@ -13,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @WebServlet("/ical-feed")
 public class ICalFeed extends HttpServlet {
@@ -23,12 +27,38 @@ public class ICalFeed extends HttpServlet {
         String id = req.getParameter("id");
         Calendar cal = dao.getCalendar(id);
         if (cal == null) { System.out.println("Calendar not found"); }
-        String icalUrl = cal.getIcsUrl();
-        ICalendar ical = Biweekly.parse(new File(icalUrl)).first();
+        File file = new File(cal.getIcsUrl());
+        new File(System.getenv("CATALINA_BASE") + "/storage/calendars").mkdirs();
+
+        if (file.createNewFile()) {
+            cal.setIcsUrl(file.getPath());
+            ICalendar ical = new ICalendar();
+            ical.setName(id);
+            ical.setDescription(id + "calendar");
+            ical.setUrl("localhost:8080/" + id + "/calendar");
+            ical.write(file);
+        }
+
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         PrintWriter out = resp.getWriter();
-        out.write(new Gson().toJson(ical.getEvents()));
+
+        TimeZone tz = TimeZone.getTimeZone("America/Chicago");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd"); // Quoted "Z" to indicate UTC, no timezone offset
+        df.setTimeZone(tz);
+        List<Map> events = new ArrayList<>();
+        ICalendar ical = Biweekly.parse(file).first();
+
+        for (VEvent event : ical.getEvents()) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("id", event.getUid().getValue());
+            map.put("start", df.format(event.getDateStart().getValue()));
+            map.put("title", event.getSummary().getValue());
+            events.add(map);
+        }
+
+        out.print(new Gson().toJson(events));
+        out.flush();
         out.close();
     }
 }
