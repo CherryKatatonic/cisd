9559486e7cafe348498f006a3a6e5d30af9fd1ca
Kat <turret24@gmail.com>
2018-09-19 10:18:45 -0500

    Temporary class for iCal testing purposes

---
+++ src/main/java/com/boulder/cisd/util/iCalTest.java
@@ -0,0 +1,133 @@
+package com.boulder.cisd.util;
+
+import biweekly.Biweekly;
+import biweekly.ICalendar;
+import biweekly.component.VEvent;
+import biweekly.property.DateStart;
+import biweekly.property.Summary;
+import biweekly.property.Uid;
+import biweekly.util.Duration;
+import biweekly.util.Frequency;
+import biweekly.util.Recurrence;
+import com.boulder.cisd.objects.Calendar;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+public class iCalTest {
+
+    public static void read() {
+        String str =
+                "BEGIN:VCALENDAR\r\n" +
+                        "VERSION:2.0\r\n" +
+                        "PRODID:-//Microsoft Corporation//Outlook 14.0 MIMEDIR//EN\r\n" +
+                        "BEGIN:VEVENT\r\n" +
+                        "UID:0123\r\n" +
+                        "DTSTAMP:20130601T080000Z\r\n" +
+                        "SUMMARY;LANGUAGE=en-us:Team Meeting\r\n" +
+                        "DTSTART:20130610T120000Z\r\n" +
+                        "DURATION:PT1H\r\n" +
+                        "RRULE:FREQ=WEEKLY;INTERVAL=2\r\n" +
+                        "END:VEVENT\r\n" +
+                        "END:VCALENDAR\r\n";
+
+//parse the first iCalendar object from the data stream
+        ICalendar ical = Biweekly.parse(str).first();
+
+//or parse all objects from the data stream
+//List<ICalendar> icals = Biweekly.parse(str).all();
+
+        VEvent event = ical.getEvents().get(0);
+        String summary = event.getSummary().getValue();
+        System.out.println(summary);
+    }
+
+    public static void write() throws IOException {
+        ICalendar ical = new ICalendar();
+        VEvent event = new VEvent();
+        Summary summary = event.setSummary("Team Meeting");
+        summary.setLanguage("en-us");
+
+        Date start = new Date();
+        event.setDateStart(start);
+
+        Duration duration = new Duration.Builder().hours(1).build();
+        event.setDuration(duration);
+
+        Recurrence recur = new Recurrence.Builder(Frequency.WEEKLY).interval(2).build();
+        event.setRecurrenceRule(recur);
+        ical.addEvent(event);
+
+        new File(System.getenv("CATALINA_BASE") + "/storage/calendars").mkdirs();
+        File file = new File(System.getenv("CATALINA_BASE") + "/storage/calendars/myCal.ics");
+        file.createNewFile();
+
+        Biweekly.write(ical).go(file);
+    }
+
+    public static Calendar createCalendar(String id) throws IOException {
+        ICalendar ical = new ICalendar();
+        ical.setName(id);
+        ical.setDescription(id + " event calendar");
+        ical.setUrl("localhost:8080/" + id + "/calendar");
+        return new Calendar(id, ical);
+    }
+
+    public static VEvent createEvent() {
+        String startDateStr = "2018-09-18";
+        String endDateStr = "2018-09-19";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = null;
+        Date endDate = null;
+
+        try {
+            startDate = sdf.parse(startDateStr);
+            endDate = sdf.parse(endDateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        VEvent e = new VEvent();
+        e.setUid(Uid.random());
+        e.setDateTimeStamp(Date.from(Instant.now()));
+        e.setSummary(new RandomString(10).nextString());
+        e.setDescription(new RandomString(30).nextString());
+        e.setDateStart(new DateStart(startDate, false));
+        e.setLocation(new RandomString(20).nextString());
+
+        return e;
+    }
+
+    public static void addEvent(Calendar cal) throws IOException {
+        String startDateStr = "2018-09-18";
+        String endDateStr = "2018-09-19";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = null;
+        Date endDate = null;
+
+        try {
+            startDate = sdf.parse(startDateStr);
+            endDate = sdf.parse(endDateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        VEvent e = new VEvent();
+        e.setUid(Uid.random());
+        e.setDateTimeStamp(Date.from(Instant.now()));
+        e.setSummary("Test Event 1");
+        e.setDescription("The first event created for testing purposes");
+        e.setDateStart(new DateStart(startDate, false));
+        e.setLocation("Tiger Stadium");
+
+        File file = new File(cal.getIcsUrl());
+        ICalendar ical = Biweekly.parse(file).first();
+        ical.addEvent(e);
+        Biweekly.write(ical).go(file);
+    }
+
+}
