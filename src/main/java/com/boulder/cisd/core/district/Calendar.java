0ab0ed84a826076652d749f728364e691796307f
Kat <turret24@gmail.com>
2018-09-19 10:15:13 -0500

    Add temporary test method for creating events

--- src/main/java/com/boulder/cisd/core/district/Calendar.java
+++ src/main/java/com/boulder/cisd/core/district/Calendar.java
@@ -1,12 +1,17 @@
 package com.boulder.cisd.core.district;
 
+import com.boulder.cisd.daos.CalendarDao;
+import com.boulder.cisd.util.iCalTest;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@MultipartConfig
 @WebServlet("/calendar")
 public class Calendar extends HttpServlet {
 
@@ -19,6 +24,10 @@ public class Calendar extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        CalendarDao dao = (CalendarDao) getServletContext().getAttribute("calDao");
+        System.out.println(req.getParameter("id"));
+        com.boulder.cisd.objects.Calendar cal = dao.getCalendar(req.getParameter("id"));
+        iCalTest.addEvent(cal);
+        resp.sendRedirect("/calendar");
     }
 }
