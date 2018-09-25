4c1bcdbf19466df8813efac967b5263d18f0019b
Kat <turret24@gmail.com>
2018-09-25 11:29:55 -0500

    Use Cloud Storage instead of local storage  - Modify URL  - Export .ics file if requested

--- src/main/java/com/boulder/cisd/util/ICalFeed.java
+++ src/main/java/com/boulder/cisd/util/ICalFeed.java
@@ -5,62 +5,91 @@ import biweekly.ICalendar;
 import biweekly.component.VEvent;
 import com.boulder.cisd.daos.CalendarDao;
 import com.boulder.cisd.objects.Calendar;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.google.gson.Gson;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-@WebServlet("/ical-feed")
+@WebServlet("/ical/*")
 public class ICalFeed extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // GET PATH INFO
+        String pathInfo = req.getPathInfo();
+        String[] pathParts = pathInfo.split("/");
+        String id = pathParts[1];
+        String scope = pathParts[2];
 
         // GET TARGET CALENDAR AND FILE URL
         CalendarDao dao = (CalendarDao) getServletContext().getAttribute("calDao");
-        String id = req.getParameter("id");
         Calendar cal = dao.getCalendar(id);
         if (cal == null) { System.out.println("Calendar not found"); }
-        File file = new File(cal.getIcsUrl());
-        new File(System.getenv("CATALINA_BASE") + "/storage/calendars").mkdirs();
+        /*File file = new File(cal.getIcsUrl());
+        new File(System.getenv("STORAGE_PATH") + "/calendars").mkdirs();*/
+        ICalendar ical;
 
-        // CREATE iCAL FILE IF ONE DOES NOT EXIST
-        if (file.createNewFile()) {
-            cal.setIcsUrl(file.getPath());
-            ICalendar ical = new ICalendar();
-            ical.setName(id);
-            ical.setDescription(id + "calendar");
-            ical.setUrl("localhost:8080/" + id + "/calendar");
-            ical.write(file);
+        // GET iCAL OBJECT
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        String blobName = cal.getBlobName();
+        Blob blob = storage.get(BlobId.of(System.getenv("BUCKET_NAME"), blobName));
+        File file = new File(System.getenv("CATALINA_TMPDIR") + "/" + id + ".ics");
+        blob.downloadTo(file.toPath());
+
+        // RETURN iCAL FILE IF REQUESTED
+        if (scope.equals("calendar")) {
+            resp.setContentType("text/calendar");
+            resp.setHeader("Content-disposition", "attachment; filename=" + id + ".ics");
+            OutputStream out = resp.getOutputStream();
+            FileInputStream in = new FileInputStream(file);
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = in.read(buffer)) > 0){
+                out.write(buffer, 0, length);
+            }
+            in.close();
+            out.flush();
+            return;
         }
+
+        /*if (file.createNewFile()) {
+            CalendarWorker.createCalendar(id);
+            ical = StorageWorker.saveCalendar(id, CalendarWorker.createCalendar(id));
+        } else {
+            ical = Biweekly.parse(file).first();
+        }*/
 
         // SET UP RESPONSE
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         PrintWriter out = resp.getWriter();
 
-        // PREPARE TO MAP iCAL EVENTS
+        // POPULATE MAP OF iCAL EVENTS
         TimeZone tz = TimeZone.getTimeZone("America/Chicago");
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         df.setTimeZone(tz);
         List<Map> events = new ArrayList<>();
-        ICalendar ical = Biweekly.parse(file).first();
+        ical = Biweekly.parse(file).first();
 
-        // MAP iCAL EVENTS INTO ARRAY
-        for (VEvent event : ical.getEvents()) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("id", event.getUid().getValue());
-            map.put("start", df.format(event.getDateStart().getValue()));
-            map.put("title", event.getSummary().getValue());
-            events.add(map);
+        if (!ical.getEvents().isEmpty()) {
+            for (VEvent event : ical.getEvents()) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", event.getUid().getValue());
+                map.put("start", df.format(event.getDateStart().getValue()));
+                map.put("title", event.getSummary().getValue());
+                // TODO - map.put(other params)...
+                events.add(map);
+            }
         }
 
         // RETURN ARRAY OF iCAL EVENTS
