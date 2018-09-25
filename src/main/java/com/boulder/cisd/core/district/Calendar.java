4a0f30b88e5039d4e62ce8f65c384a67b8a4beb2
Kat <turret24@gmail.com>
2018-09-25 11:16:19 -0500

    Create new Calendar if one does not exist

--- src/main/java/com/boulder/cisd/core/district/Calendar.java
+++ src/main/java/com/boulder/cisd/core/district/Calendar.java
@@ -1,7 +1,8 @@
 package com.boulder.cisd.core.district;
 
+import biweekly.ICalendar;
 import com.boulder.cisd.daos.CalendarDao;
-import com.boulder.cisd.util.iCalTest;
+import com.boulder.cisd.util.CalendarWorker;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -17,6 +18,21 @@ public class Calendar extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CalendarDao dao = (CalendarDao) getServletContext().getAttribute("calDao");
+
+        if (dao.getCalendar("district") == null) {
+            ICalendar ical = CalendarWorker.createCalendar("district");
+            com.boulder.cisd.objects.Calendar cal = null;
+
+            try {
+                cal = new com.boulder.cisd.objects.Calendar("district", ical);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+            dao.saveCalendar(cal);
+        }
+
         req.setAttribute("campus", "district");
         req.setAttribute("page", "calendar");
         req.getRequestDispatcher("/index.jsp").forward(req, resp);
@@ -25,9 +41,18 @@ public class Calendar extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         CalendarDao dao = (CalendarDao) getServletContext().getAttribute("calDao");
-        System.out.println(req.getParameter("id"));
         com.boulder.cisd.objects.Calendar cal = dao.getCalendar(req.getParameter("id"));
-        iCalTest.addEvent(cal);
+        /*File file = new File(cal.getIcsUrl());
+        ICalendar ical = Biweekly.parse(file).first();
+        VEvent e = null;
+
+        try {
+            e = CalendarWorker.createEvent(req, resp);
+        } catch (ParseException e1) {
+            e1.printStackTrace();
+        }
+
+        ical.addEvent(e);*/
         resp.sendRedirect("/calendar");
     }
 }
