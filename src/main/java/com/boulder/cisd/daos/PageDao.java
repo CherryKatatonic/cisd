3e1241442d9cf0c0470b3c147e81e96a1b3ab288
Kat <turret24@gmail.com>
2018-10-10 10:05:52 -0500

    Add updatePage(), rename savePage() > createPage()

--- src/main/java/com/boulder/cisd/daos/PageDao.java
+++ src/main/java/com/boulder/cisd/daos/PageDao.java
@@ -7,13 +7,17 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 
 public class PageDao {
 
-    public Page savePage(Page page) {
+    public Page createPage(Page page) {
         Key<Page> key = ofy().save().entity(page).now();
         return ofy().load().key(key).now();
     }
 
     public Page getPage(String id) {
         return ofy().load().key(Key.create(Page.class, id)).now();
+    }
+
+    public void updatePage(Page page) {
+        ofy().save().entity(page).now();
     }
 
     public void deletePage(String id) {
