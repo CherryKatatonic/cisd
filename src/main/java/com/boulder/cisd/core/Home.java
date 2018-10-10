e769d30a130ad02d8e0481142e5bd91906f647cf
Kat <turret24@gmail.com>
2018-10-10 09:31:40 -0500

    Set @Path methods

--- src/main/java/com/boulder/cisd/core/Home.java
+++ src/main/java/com/boulder/cisd/core/Home.java
@@ -1,17 +1,30 @@
 package com.boulder.cisd.core;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import java.io.IOException;
 
-@WebServlet
+@Path("/")
 public class Home extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    @GET
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("campus", "district");
+        req.setAttribute("page", "home");
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
     }
+
+    @GET @Path("/{campus}")
+    public void doGet(@PathParam("campus") String campus, HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("campus", campus);
+        req.setAttribute("page", "home");
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+
 }
