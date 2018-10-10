7bfee1f63304d0a5837c98cda609e73c449b9864
Kat <turret24@gmail.com>
2018-10-10 10:03:41 -0500

    Create Page Dao

---
+++ src/main/java/com/boulder/cisd/daos/PageDao.java
@@ -0,0 +1,23 @@
+package com.boulder.cisd.daos;
+
+import com.boulder.cisd.objects.Page;
+import com.googlecode.objectify.Key;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+public class PageDao {
+
+    public Page savePage(Page page) {
+        Key<Page> key = ofy().save().entity(page).now();
+        return ofy().load().key(key).now();
+    }
+
+    public Page getPage(String id) {
+        return ofy().load().key(Key.create(Page.class, id)).now();
+    }
+
+    public void deletePage(String id) {
+        ofy().delete().key(Key.create(Page.class, id)).now();
+    }
+
+}
