8a0bf9e0cab842447bd070fc44213d2e146dfc96
Kat <turret24@gmail.com>
2018-10-10 09:38:21 -0500

    Add components list

--- src/main/java/com/boulder/cisd/objects/Page.java
+++ src/main/java/com/boulder/cisd/objects/Page.java
@@ -3,22 +3,32 @@ package com.boulder.cisd.objects;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
+import java.util.List;
+
 @Entity
 public class Page {
 
     @Id private String id;
+    private List components;
 
     private Page() {}
 
     private Page(Builder builder) {
         this.id = builder.id;
+        this.components = builder.components;
     }
 
     public static class Builder {
         private String id;
+        private List components;
 
         public Page.Builder id(String id) {
             this.id = id;
+            return this;
+        }
+
+        public Page.Builder components(List components) {
+            this.components = components;
             return this;
         }
 
@@ -33,5 +43,13 @@ public class Page {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public List getComponents() {
+        return components;
+    }
+
+    public void setComponents(List components) {
+        this.components = components;
     }
 }
